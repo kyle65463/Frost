325f5f6af7778a6b427eb48a9a5a1cba71f31fc9
kyle65463 <kyle65463@gmail.com>
2021-03-26 17:39:06 +0800

    feat: complete premake configuration

---
+++ premake5.lua
@@ -0,0 +1,106 @@
+workspace "Frost"
+    architecture "x64"
+
+    configurations
+    {
+        "Debug",
+        "Release",
+        "Dist"
+    }
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Frost"
+    location "Frost"
+    kind "SharedLib"
+    language "C++"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp",
+    }
+
+    includedirs
+    {
+        "%{prj.name}/vendor/spdlog/include",
+    }
+
+    filter "system:windows"
+        cppdialect "C++17"
+        staticruntime "On"
+        systemversion "latest"
+
+        defines
+        {
+            "FS_PLATFORM_WINDOWS",
+            "FS_BUILD_DLL"
+        }
+
+        postbuildcommands 
+        {
+            ("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+        }
+    
+        filter "configurations:Debug"
+            defines "FS_DEBUG"
+            symbols "On"
+        
+        filter "configurations:Release"
+            defines "FS_RELEASE"
+            optimize "On"
+
+        filter "configurations:Dist"
+            defines "FS_DIST"
+            optimize "On"
+
+project "Sandbox"
+    location "Sandbox"
+    kind "ConsoleApp"
+    language "C++"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp",
+    }
+
+    includedirs
+    {
+        "Frost/vendor/spdlog/include",
+        "Frost/src"
+    }
+
+    links
+    {
+        "Frost"
+    }
+
+    filter "system:windows"
+        cppdialect "C++17"
+        staticruntime "On"
+        systemversion "latest"
+
+        defines
+        {
+            "FS_PLATFORM_WINDOWS",
+        }
+
+        filter "configurations:Debug"
+            defines "FS_DEBUG"
+            symbols "On"
+        
+        filter "configurations:Release"
+            defines "FS_RELEASE"
+            optimize "On"
+
+        filter "configurations:Dist"
+            defines "FS_DIST"
+            optimize "On"
+    
